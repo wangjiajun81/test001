b832e64f919bb4006c67129848adfd0776db1e3a
wang.jiajun <wang.jiajun@xiji.com>
2017-05-16 18:18:54 +0800

    lp.py

---
+++ lp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print (100+200+300)
+print ('hello,world')
+print('1024*768 =',1024*768)
+name=input('please input your name:')
+print('hello,',name)
