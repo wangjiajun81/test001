9e238ce3ebdb422a2795fc089d19fa3311d9fa1b
wang.jiajun <wang.jiajun@xiji.com>
2017-05-17 09:08:47 +0800

    0517

--- lp.py
+++ lp.py
@@ -5,3 +5,20 @@ print ('hello,world')
 print('1024*768 =',1024*768)
 name=input('please input your name:')
 print('hello,',name)
+print('I\'m ok!')
+a = 123 # a是整数
+print(a)
+a = 'ABC' # a变为字符串
+print(a)
+n = 123
+f = 456.789
+s1 = "'Hello,world'"
+s2 = "'Hello,\\'Adam\\''"
+s3 = "r\'Hello,\"Bart\"\'"
+s4 = "r\'\'\'Hello,\nLisa!\'\'\'"
+print('n =',n)
+print('f =',f)
+print('s1 =',s1)
+print('s2 =',s2)
+print('s3 =',s3)
+print('s4 =',s4)
